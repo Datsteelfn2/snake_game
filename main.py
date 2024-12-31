b6cc7163e5283c343b791054051805ac5a50d81c
Datsteelfn2 <Datsteelfn@gmail.com>
2024-12-31 10:02:15 -0500

    created snake class and image on screen

--- main.py
+++ main.py
@@ -1,14 +1,23 @@
-import pygame
+import pygame,random
 from pygame.math import Vector2
+
+
+class SNAKE:
+    def __init__(self):
+        self.body=[Vector2(5,10),Vector2(6,10),Vector2(7,10)]
+    def draw_snake(self):
+        for block in self.body:
+            blcok_rect=pygame.Rect((block.x*cell_size),(block.y*cell_size),cell_size,cell_size)
+            pygame.draw.rect(window,(183,111,122),blcok_rect)
 class FRUIT:
     def __init__(self):
         #x and y position
         #draw a square(fruit)
-        self.x=5
-        self.y=4
-        self.position=pygame.math.Vector2(self.x,self.y)
+        self.x=random.randint(0,cell_number-1)
+        self.y=random.randint(0,cell_number-1)
+        self.position=Vector2(self.x,self.y)
     def draw_fruit(self):
-        fruit_rect=pygame.Rect(self.position.x,self.position.y,cell_size,cell_size)
+        fruit_rect=pygame.Rect(self.position.x*cell_size,self.position.y*cell_size,cell_size,cell_size)
         pygame.draw.rect(window,(126,166,114),fruit_rect)
 
 pygame.init()
@@ -18,7 +27,7 @@ cell_number=20
 window=pygame.display.set_mode((cell_size*cell_number,cell_size*cell_number))
 
 fruit=FRUIT()
-
+snake=SNAKE()
 clock=pygame.time.Clock()
 
 run=True
@@ -29,5 +38,6 @@ while run:
 
     window.fill((175,215,70))
     fruit.draw_fruit()
+    snake.draw_snake()
     clock.tick(60)
     pygame.display.update()
